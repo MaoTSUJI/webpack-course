90cdf32da69021fefc10cd3eb4e2dca96bb6300c
MaoTSUJI <tsuji.map36@gmail.com>
2021-05-23 20:44:24 +0900

    Generate HTML through Pug

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,21 @@ module.exports = {
           //     name: 'images/[name].[ext]',
           //   }
           // },
-        ]
+        ],
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            },
+          },
+        ],
       },
     ],
   },
@@ -45,7 +59,12 @@ module.exports = {
       filename: './stylesheets/main.css'
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.html',
+      template: './src/templates/index.pug',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/access.pug',
+      filename: 'access.html',
     }),
     new CleanWebpackPlugin(),
   ],
